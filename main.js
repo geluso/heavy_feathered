47ba5dc4447333c4beaa3128e2fb9ec3476295e1
Steve Geluso <stevegeluso@gmail.com>
2019-08-12 17:38:07 -0700

    adding cars to lanes when they're created

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const WIDTH = 200
-const LANES = 4
-const HALF_LANE = WIDTH / LANES / 2
+const NUM_LANES = 4
+const HALF_LANE = WIDTH / NUM_LANES / 2
 const HEIGHT = 600
 
 const CAR_WIDTH = 20
@@ -10,12 +10,19 @@ const NUM_INITIAL_CARS = 30
 import Car from './car.js'
 
 let CARS = []
+let LANES = {}
+for (let i = 0; i < NUM_LANES; i++) {
+  LANES[i] = []
+}
 
 document.addEventListener('DOMContentLoaded', main)
 function main() {
   for (let i = 0; i < NUM_INITIAL_CARS; i++) {
-    let car = randomCar()                  
-    CARS.push(car)
+    generateCar()
+  }
+
+  for (let key in LANES) {
+    LANES[key].sort(compareLanePosition)  
   }
 
   let canvas = document.getElementById('glass')
@@ -27,6 +34,8 @@ function main() {
   ctx.width = WIDTH
   ctx.height = HEIGHT
 
+  tick(ctx);
+  return;
   setInterval(() => tick(ctx), 1000 / 60)
 }
 
@@ -42,11 +51,17 @@ function draw(ctx) {
   CARS.forEach(car => drawCar(ctx, car))
 }
 
+function generateCar(yy) {
+  let {car, lane} = randomCar(yy)
+  LANES[lane].push(car)
+  CARS.push(car)
+}
+
 function tick(ctx) {
   CARS.forEach(car => {
     car.tick()
     if (car.yy < -CAR_HEIGHT) {
-      CARS.push(randomCar(HEIGHT - CAR_HEIGHT))
+      generateCar(HEIGHT - CAR_HEIGHT)
     }
   })
 
@@ -67,7 +82,8 @@ function randomCar(yy) {
     4 * WIDTH / 4 - HALF_LANE,
   ]
 
-  let xx = lanesX[Math.floor(lanesX.length * Math.random())]
+  let lane = Math.floor(lanesX.length * Math.random())
+  let xx = lanesX[lane]
   yy = yy || HEIGHT * Math.random()
 
   // have the car drive between 55-80 MPH scaled to where 10 represents 60 MPH
@@ -76,5 +92,9 @@ function randomCar(yy) {
   let speed = (minSpeed + (maxSpeed - minSpeed) * Math.random()) / 10
 
   const car = new Car(xx, yy, speed)
-  return car
+  return {car, lane}
+}
+
+function compareLanePosition(car1, car2) {
+  return car2.yy - car1.yy
 }
